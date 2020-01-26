70d7cc3a6aca60bf5b9af373d120524694dbe21b
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-25 22:17:03 -0800

    Update Book Model to use correct Join Model Assocation

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
     has_many :bookgenres
     has_many :genres, through: :bookgenres
     has_many :bookusers
-    has_many :readers, foreign_key: "user_id", class_name: "User"
+    has_many :users, through: :bookusers
     has_many :reviews
 
 end
