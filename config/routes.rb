2eef000a7fddca4300c3c62eee3a73bcf8ec8045
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-26 12:23:35 -0800

    Add routes for Genres

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :reviews
   resources :books
   resources :users do
-    resources :books, only [:index]
+    resources :books, only: [:index]
   end
+  resources :genres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "books#index"
 end
