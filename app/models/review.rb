2914fac0430695ece35706802988f3012d9c3769
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-26 15:13:59 -0800

    Add validations to Review Model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :score, numericality: {greater_than: 0, less_than_or_equal_to: 10}
+  validates :content, length: { in: 20...250 }
 end
