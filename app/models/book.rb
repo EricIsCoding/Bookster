a347dbb7259fcf959e1739353b55effacf1501be
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-25 22:06:21 -0800

    Add Associations to Book Model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+    has_many :bookgenres
+    has_many :genres, through: :bookgenres
+    has_many :bookusers
+    has_many :readers, foreign_key: "user_id", class_name: "User"
+    has_many :reviews
+
 end
