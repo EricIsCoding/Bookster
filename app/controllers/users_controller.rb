942e3643a881495b1be6fc0855b348b846ad8070
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-27 01:05:45 -0800

    Add Show Action for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def show
+        if current_user.id != params[:id]
+            flash[:alert] = "That is not your Profile. Don't do that."
+            redirect_to "/"
+        else
+            @user = current_user
+        end
+    end
 end
