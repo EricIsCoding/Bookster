32cf24d92dd27a9044cddc7e61551031d47a0844
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-26 20:57:25 -0800

    Add Basic Controller Actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,32 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.create(review_params)
+        if @review.persisted?
+            redirect_to book_path(@review.book)
+        end
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:score, :content, :book_id, :user_id)
+    end
+    
 end
