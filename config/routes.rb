52303ec680dec0c83953f363bea85852258a455f
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-26 22:23:48 -0800

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+  resources :reviews, only: [:create, :update, :destroy]
   resources :books do 
     resources :genres, only: [:new]
     resources :reviews, only: [:new, :edit]
   end
   resources :users do
     resources :books, only: [:index]
+    resources :reviews, only: [:show]
   end
   resources :genres
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
