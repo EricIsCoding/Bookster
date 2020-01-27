c0ab792177d20f59edebe0c99d9fbfc32fbb97d8
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-26 21:44:04 -0800

    Add Helper To validate Record Owner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     def update_params(params)
         params.reject{|k, v| v.blank?}
     end
+
+    def record_owner?(record)
+      record.user === current_user
+    end
 end
