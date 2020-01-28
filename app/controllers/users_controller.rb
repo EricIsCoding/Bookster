a70db4f73ae6bcd833eed72c9aa0c1eba4f647e4
Eric J Wilhite <EricJWilhite@gmail.com>
2020-01-27 19:50:48 -0800

    Fix bug caused by a datatype issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def show
-        if current_user.id != params[:id]
+        if current_user.id != params[:id].to_i
             flash[:alert] = "That is not your Profile. Don't do that."
             redirect_to "/"
         else
